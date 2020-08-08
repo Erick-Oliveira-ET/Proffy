7b98f24a6630a83884833f45b2f5a7ac5e059514
Erick Oliveira <erickoliveirasystem@outlook.com>
2020-08-08 16:15:18 -0300

    Add new schedule on TeacheerForm

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -15,8 +15,8 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest}) => {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select typeof="text" id={name} {...rest}>
-                <option value="" hidden disabled selected>Selecione uma matéria</option>
+            <select defaultValue="" id={name} {...rest}>
+                <option value="" hidden disabled >Selecione uma matéria</option>
                 {options.map(option => {
                     return <option key={option.value} value={option.value}>{option.label}</option>
                 })}
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import TextArea from '../../components/TextArea';
@@ -9,6 +10,23 @@ import warningIcon from '../../assets/icons/warning.svg'
 import './styles.css'
 
 function TeacherForm() {
+    //scheduleItems are the values of the state and the setScheduleItems
+    //is a that replace the scheduleItems because a React state can't 
+    //be changed
+    const [scheduleItems, setScheduleItems] = useState([
+        {week_day: 0, from:'', to:''}
+    ]);
+
+    //Because it`s not possible to change the variable, 
+    //this function pass all the existing variables on the scheduleItems
+    //and pass the new ones to reset the scheduleItems variable
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems,
+            {week_day: 0, from:'', to:''}
+        ]);
+    }
+
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader title="Que incrivel que voce quer dar aula."
@@ -46,28 +64,33 @@ function TeacherForm() {
 
                 <fieldset>
                     <legend>Horários Disponiveis
-                        <button type="button">
+                        <button type="button" onClick={addNewScheduleItem}>
                             + Novo Horário
                         </button>
                         
                     </legend>
 
-                    <div className="schedule-item">
-                        <Select name="subject" label="Matéria" 
-                            options={[
-                                {value: "0", label:"Domingo"},
-                                {value: "1", label:"Segunda-feira"},
-                                {value: "2", label:"Terça-feira"},
-                                {value: "3", label:"Quarta-feira"},
-                                {value: "4", label:"Quinta-feira"},
-                                {value: "5", label:"Sexta-feira"},
-                                {value: "6", label:"Sábado"}
-                            ]}
-                        />
+                   {scheduleItems.map(scheduleItem => {
+                        return(
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                                <Select name="subject" label="Matéria" 
+                                    options={[
+                                        {value: "0", label:"Domingo"},
+                                        {value: "1", label:"Segunda-feira"},
+                                        {value: "2", label:"Terça-feira"},
+                                        {value: "3", label:"Quarta-feira"},
+                                        {value: "4", label:"Quinta-feira"},
+                                        {value: "5", label:"Sexta-feira"},
+                                        {value: "6", label:"Sábado"}
+                                    ]}
+                                />
 
-                        <Input name="from" label="Das" type="time" />
-                        <Input name="to" label="às" type="time" />
-                    </div>
+                                <Input name="from" label="Das" type="time" />
+                                <Input name="to" label="às" type="time" />
+                            </div>
+                        )
+                   })}
+
                 </fieldset>
                 <footer>
                     <p>
